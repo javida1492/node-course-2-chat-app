aebd44bd0a26edee0834353607a9214b2183f9e7
Andrew Javidfar <javida1492@gmail.com>
2019-01-03 21:40:13 -0500

    Add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,28 @@
+var socket = io();
+
+socket.on("connect", function() {
+  console.log("Connected to server");
+
+  /*socket.emit("createEmail", {
+    to: "uri@example.com",
+    text: "This is a test msg"
+  });*/
+
+  socket.emit("createMessage", {
+    from: "Andrew",
+    text: "That works"
+  });
+
+});
+
+socket.on("disconnect", function() {
+  console.log("Disconnected from server");
+});
+
+/*socket.on("newEmail", function (email) {
+  console.log("New email", email);
+});*/
+
+socket.on("newMessage", function(message) {
+  console.log("newMessage", message);
+});
--- server/server.js
+++ server/server.js
@@ -1,36 +1,49 @@
-//----------------------------------------------------------------------------//
-//                            APIS
-const socketIO = require("socket.io");
+//--------------------------------- APIS -------------------------------------//
 const path     = require("path");
-const express  = require("express");
 const http     = require("http");
-//----------------------------------------------------------------------------//
+const express  = require("express");
+const socketIO = require("socket.io");
+//------------------------------ CONSTANTS -----------------------------------//
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;  //Configure port connection
-//----------------------------------------------------------------------------//
-//                         VARIABLES
+//------------------------------ VARIABLES -----------------------------------//
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);            //configure socket to use our webserver
 //----------------------------------------------------------------------------//
-
-
 app.use(express.static(publicPath));
 
 //This function lets us register an event listener for a new connection and respond to it
 io.on("connection", (socket) => {
   console.log("New user connection");
 
+  /*socket.emit("newEmail", {
+    from: "andrew@example.com",
+    text: "This is a default msg",
+    createdAt: 123
+  });*/
+
+  socket.emit("newMessage", {
+    from: "Uri",
+    text: "Is this a good trade?",
+    createdAt: 123123
+  });
+
+  socket.on("createMessage", (message) => {
+    console.log("createMessage", message);
+  });
+
+  /*(socket.on("createEmail", (newEmail) => {
+    console.log("createEmail", newEmail);
+  });*/
+
   socket.on("disconnect", () => {
     console.log("User was disconnected");
   });
 });
-
-
-
 //----------------------------------------------------------------------------//
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
 
-module.exports = {app};
+//module.exports = {app};
