52c5b98a1dd4306b24bc6e56b34671cd549ace61
Andrew Javidfar <javida1492@gmail.com>
2019-01-03 20:48:38 -0500

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,18 +1,36 @@
-const path = require("path");
-const express = require("express");
-
+//----------------------------------------------------------------------------//
+//                            APIS
+const socketIO = require("socket.io");
+const path     = require("path");
+const express  = require("express");
+const http     = require("http");
+//----------------------------------------------------------------------------//
 const publicPath = path.join(__dirname, "../public");
-
+const port = process.env.PORT || 3000;  //Configure port connection
+//----------------------------------------------------------------------------//
+//                         VARIABLES
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);            //configure socket to use our webserver
+//----------------------------------------------------------------------------//
 
-const port = process.env.PORT || 3000;//process.env.PORT;
 
 app.use(express.static(publicPath));
 
+//This function lets us register an event listener for a new connection and respond to it
+io.on("connection", (socket) => {
+  console.log("New user connection");
+
+  socket.on("disconnect", () => {
+    console.log("User was disconnected");
+  });
+});
+
+
+
+//----------------------------------------------------------------------------//
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
-
-console.log(publicPath);
 
 module.exports = {app};
